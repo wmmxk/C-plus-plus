f8506ba50a71da6ed6302fce9eb64c873cf54825
wmmxk <wmmkx@hotmail.com>
2017-06-27 09:36:26 -0700

    assign and condition on the assignment, while loop

---
+++ condition/assign_condition.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+
+{
+ int	y[4] = {1,2,3,4};
+	int x;
+	int i=0;
+	while ((x = y[i++])!=4) {
+ cout << y[i] << endl;
+	}
+// cout << x=30 << endl;
+}
