117ca904a033fff911f21d1539a86cc592cd8953
wmmxk <wmmkx@hotmail.com>
2017-08-20 08:35:01 -0700

    autoconfig, generate Makefile automatically

---
+++ makefile/autoconfig/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+// source: https://robots.thoughtbot.com/the-magic-behind-configure-make-make-install
+int main(int argc, char* argv[])
+{
+   printf("Hello world: %s\n",argv[1]);
+   return 0;
+
+}
