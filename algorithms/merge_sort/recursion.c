b149784b32b03cbd6be0730385023c6395f52b68
wmmxk <wmmkx@hotmail.com>
2017-10-10 08:49:13 -0700

    recode common algorithms in c, merege_sort

---
+++ algorithms/merge_sort/recursion.c
@@ -0,0 +1,46 @@
+/*
+ *source: https://vinayakgarg.wordpress.com/2012/11/08/merge-sort-program-in-c/
+ */
+
+/* 
+ *We need 2 arrays 'a' and 'b', of equal size
+ *Here 'a' is the primary array (i.e which contains initial 
+     input, and final output)
+ *And 'b' is the temporary array,
+     used for merging 2 sorted half's in 'a' 
+ */
+  
+void merge(int a[], int low, int mid, int high)
+{
+    int b = malloc(sizeof(int)(high-low+1));
+    int i = low, j = mid + 1, k = 0;
+  
+    while (i <= mid && j <= high) {
+        if (a[i] <= a[j])
+            b[k++] = a[i++];
+        else
+            b[k++] = a[j++];
+    }
+    while (i <= mid)
+        b[k++] = a[i++];
+  
+    while (j <= high)
+        b[k++] = a[j++];
+  
+    k--;
+    while (k >= 0) {
+        a[low + k] = b[k];
+        k--;
+    }
+	free(b)
+}
+  
+void mergesort(int a[], int low, int high)
+{
+    if (low < high) {
+        int m = (high + low)/2;
+        mergesort(a, low, m);
+        mergesort(a, m + 1, high);
+        merge(a, low, m, high);
+    }
+}
