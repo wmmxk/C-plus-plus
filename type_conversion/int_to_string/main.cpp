431f617dddbfd825bca7b6a2492483e75c04464f
wmmxk <wmmkx@hotmail.com>
2017-10-28 22:37:42 -0700

    hash, int_2_string,

---
+++ type_conversion/int_to_string/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+int main() {
+ int a = 123;
+ std::string str = std::to_string(a);
+
+ std::cout << "a string"
+		    << str
+						<< "\n";
+
+
+ return 0;
+
+}
