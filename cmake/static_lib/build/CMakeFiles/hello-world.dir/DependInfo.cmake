75df65e4b73b9c75d7cf3f6eee12e19e1859e1a0
wmmxk <kanwang@ucdavis.edu>
2019-03-01 17:11:43 -0800

    cmake basic; stack library; NMS opencv

---
+++ cmake/static_lib/build/CMakeFiles/hello-world.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/wxk/Git/C-plus-plus/cmake/static_lib/hello-world.cpp" "/home/wxk/Git/C-plus-plus/cmake/static_lib/build/CMakeFiles/hello-world.dir/hello-world.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/wxk/Git/C-plus-plus/cmake/static_lib/build/CMakeFiles/message.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
